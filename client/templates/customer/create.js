473bca9658415d5c1f401887f31c7ba5b9deef55
Luiz Henrique Pegoraro <lpegoraro@localhost.localdomain>
2015-07-20 22:39:33 -0300

    Fixed ui

--- client/templates/customer/create.js
+++ client/templates/customer/create.js
@@ -1,12 +1,15 @@
 Template.customerCreate.events({
-  'submit form' : function () {
-    var customerName = $('.name').val();
-    if(_.isEmpty(customerName) || _.isNull(customerName) || _.isUndefined(customerName)) {
+  'submit form': function() {
+    var customerName = $('.name').val(),
+      customerPhone = $('.phone').val();
+    if (_.isEmpty(customerName) || _.isNull(customerName) || _.isUndefined(
+        customerName)) {
       sAlert.error('Insira o nome do cliente!');
       $('#customer-name').focus();
     } else {
       Customers.insert({
-        name: customerName
+        name: customerName,
+        phone: customerPhone
       });
     }
   }
