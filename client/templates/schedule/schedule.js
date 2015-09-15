c974ea3638c21a3eaa12eeb385573419844c76d4
Luiz Henrique Pegoraro <luizhp@ciandt.com>
2015-09-14 23:37:06 -0300

    Added schedule handlers

--- client/templates/schedule/schedule.js
+++ client/templates/schedule/schedule.js
@@ -1,8 +1,26 @@
 Template.schedule.helpers({
     options: function() {
         return {
-            defaultView: 'basicWeek',
-            schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source'
+          header: {
+      				left: 'prev,next today',
+      				center: 'title',
+      				right: 'month,agendaWeek,agendaDay'
+    			},
+          id: 'myCalendar',
+    			editable: true,
+          defaultView: 'agendaWeek',
+          header : {center: 'month,agendaWeek,agendaDay'},
+          weekends:false,
+          schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+          eventClick : function () {
+            console.log('clickei no evento!!');
+            //Open Event Edit Mode
+          },
+          dayClick: function (e) {
+            console.log(e);
+            $('#myCalendar').fullCalendar( 'changeView', 'agendaDay');
+            $('#myCalendar').fullCalendar('gotoDate', e._d);
+          }
         };
     }
 });
