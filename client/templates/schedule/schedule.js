957e7d0b7470654c93f04f89c6e908489d6b927d
Luiz Henrique Pegoraro <luizhp@ciandt.com>
2015-09-24 00:13:53 -0300

    Added new steps into full calendar

--- client/templates/schedule/schedule.js
+++ client/templates/schedule/schedule.js
@@ -17,9 +17,17 @@ Template.schedule.helpers({
             //Open Event Edit Mode
           },
           dayClick: function (e) {
-            console.log(e);
-            $('#myCalendar').fullCalendar( 'changeView', 'agendaDay');
-            $('#myCalendar').fullCalendar('gotoDate', e._d);
+            var date = e._d,
+                $calendar = $('#myCalendar');
+            if($calendar.fullCalendar('getView').name != 'agendaDay') {
+              if(e.hour() === 0 && e.minute() === 0) {
+                date = e.add(1,'hours');
+              }
+              $calendar.fullCalendar( 'changeView', 'agendaDay');
+              $calendar.fullCalendar('gotoDate', date);
+            } else {
+              //go to add event
+            }
           }
         };
     }
