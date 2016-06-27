d61caf110d7660fb42c91e0bf6d1ad84021045b0
lpegora <luizhp@ciandt.com>
2016-06-26 21:29:57 -0300

    Added bootstrap datetime picker into modal

--- client/templates/schedule/eventAdd/eventAdd_script.js
+++ client/templates/schedule/eventAdd/eventAdd_script.js
@@ -1,7 +1,19 @@
-Template.customerCombo.onRendered( function () {
-  var customers = Customers.find({}, {name : 1}).fetch();
+Template.customerCombo.onRendered(function () {
+  var customers = Customers.find({}, {
+    name: 1
+  }).fetch();
   var option = $('')
+  console.log('Customers: ', customers);
   _.each(customers, function (customer) {
-
+    console.log('Customer: ', customer);
   });
+  $('#datetimepicker').datetimepicker({
+    format: 'DD/MM/YYYY HH:mm',
+    locale: 'pt-br',
+    useCurrent: true,
+    collapse: true,
+    daysOfWeekDisabled: [6, 7],
+    disabledHours: [0, 18, 19, 20, 21, 22, 23, 24]
+  });
+
 });
