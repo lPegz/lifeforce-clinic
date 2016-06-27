8bd625ba7b7152a3aba71115c26c0fc1bf380fb9
lpegora <luizhp@ciandt.com>
2016-06-26 23:00:57 -0300

    Add publishs and subscriptions

--- server/publishs.js
+++ server/publishs.js
@@ -1,34 +1,20 @@
-Meteor.publish('customers', function() {
-  return Customers.find();
+Meteor.publish('customers.public', function () {
+  return Customers.find({}, {
+    fields: Customers.name
+  });
 });
-Meteor.publish('treatments', function() {
+Meteor.publish('treatments.public', function () {
   return Treatments.find();
 });
-Meteor.publish('sessions', function() {
+Meteor.publish('sessions.public', function () {
   return Sessions.find();
 });
 
 Router.configure({
   layoutTemplate: 'layout',
-  // subscribe to our animals publication
-  // with a waitOn function in Iron Router
-  // ... now our application will wait to load
-  // until we've successfully subscribed to the
-  // publication
   waitOn: function () {
-    Meteor.subscribe('customers');
-    Meteor.subscribe('treatments');
-    Meteor.subscribe('sessions');
+    Meteor.subscribe('customers.public');
+    Meteor.subscribe('treatments.public');
+    Meteor.subscribe('sessions.public');
   }
-});
-Router.route('/api/customers', {
-  name: 'customers'
-});
-
-Router.route('/api/treatments', {
-  name: 'treatments'
-});
-
-Router.route('/api/sessions', {
-  name: 'sessions'
 });
