fa3106cc6695abbe63089d89ed14270dade73f18
Luiz Henrique Pegoraro <luizhp@ciandt.com>
2016-10-04 17:01:51 -0300

    Added new version of outdated libs

--- client/templates/schedule/schedule.js
+++ client/templates/schedule/schedule.js
@@ -1,14 +1,3 @@
-var testEvents = function () {
-  return [{
-    title: 'TESTE',
-    start: moment()._d,
-    end: moment().add(1, 'hours')._d
-  }, {
-    title: 'TESTE2',
-    start: moment().add(1, 'day')._d,
-    end: moment().add(1, 'day').add(1, 'hours')._d
-  }]
-};
 var actualEvents = function () {
   Meteor.subscribe('sessions.public');
   var sessions = PSessions.find().fetch();
@@ -24,48 +13,9 @@ var actualEvents = function () {
   });
 };
 
-Template.schedule.helpers({
-  options: function () {
-    Meteor.subscribe('sessions.public');
-    var sessions = PSessions.find().fetch();
-    console.log(actualEvents());
-    return {
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-      },
-      id: 'myCalendar',
-      editable: true,
-      defaultView: 'agendaWeek',
-      header: {
-        center: 'month,agendaWeek,agendaDay'
-      },
-      allDaySlot: false,
-      minTime: '07:00:00',
-      maxTime: '21:00:00',
-      events: actualEvents(),
-      lang: 'pt-br',
-      slotEventOverlap: false,
-      weekends: false,
-      schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
-      dayClick: function (e) {
-        var date = e._d,
-          $calendar = $('#myCalendar');
-        if ($calendar.fullCalendar('getView').name != 'agendaDay') {
-          if (e.hour() === 0 && e.minute() === 0) {
-            date = e.add(1, 'hours');
-          }
-          $calendar.fullCalendar('changeView', 'agendaDay');
-          $calendar.fullCalendar('gotoDate', date);
-        } else {
-          //popup modal
-          Modal.show('modalAddEvent');
-          $('body').data('datetime', e);
-        }
-      }
-    };
-  }
+Template.schedule.onCreated( () => {
+  let template = Template.instance();
+  template.subscribe('sessions.public');
 });
 
 Template.schedule.events({
@@ -73,4 +23,42 @@ Template.schedule.events({
     Modal.show('modalAddEvent');
     $('body').data('datetime', e);
   }
-})
+});
+
+Template.schedule.onRendered( function () {
+  $('#events-calendar').fullCalendar( {
+    header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+    id: '',
+    editable: true,
+    defaultView: 'agendaWeek',
+    header: {
+      center: 'month,agendaWeek,agendaDay'
+    },
+    allDaySlot: false,
+    minTime: '07:00:00',
+    maxTime: '21:00:00',
+    lang: 'pt-br',
+    slotEventOverlap: false,
+    weekends: false,
+    schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+    dayClick: function (e) {
+      var date = e._d,
+        $calendar = $('#events-calendar');
+      if ($calendar.fullCalendar('getView').name != 'agendaDay') {
+        if (e.hour() === 0 && e.minute() === 0) {
+          date = e.add(1, 'hours');
+        }
+        $calendar.fullCalendar('changeView', 'agendaDay');
+        $calendar.fullCalendar('gotoDate', date);
+      } else {
+        //popup modal
+        Modal.show('modalAddEvent');
+        $('body').data('datetime', e);
+      }
+    }
+  }
+)});
